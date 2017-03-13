d5c54b32be0fc40ba9586117850790cacf891a46
griffgruffjava <ciaran@ciarangriffin.com>
2017-03-13 10:18:41 +0000

    first commit for bot tut

---
+++ HotelBot/Dialogs/GreetingDialog.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+
+namespace HotelBot.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Hi I'm Ciaran Bot");
+            context.Wait(MessageRecievedAsync);
+          
+        }
+
+        public virtual async Task MessageRecievedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var userName = String.Empty;
+            var getName = false;
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if(getName)
+            {
+                userName = message.Text;
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", getName);
+            }
+
+
+            if(string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("What is your name pal?");
+                userName = message.Text;
+                context.UserData.SetValue<string>("Name", userName);
+
+            }
+            else
+            {
+                await context.PostAsync(String.Format("Hi {0}.  How can I help you today", userName));
+            }
+            context.Wait(MessageRecievedAsync);
+        }
+    }
+} 
